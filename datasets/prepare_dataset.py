6b63314d782791fa8d3e58a5e88c15094eed63c1
batectin <12725557+batectin@users.noreply.github.com>
2024-08-17 21:23:50 +0800

    Update prepare_dataset.py

--- datasets/prepare_dataset.py
+++ datasets/prepare_dataset.py
@@ -7,7 +7,7 @@ import json
 #import tensorflow as tf
 import tensorflow.compat.v1 as tf
 tf.disable_v2_behavior()
-from scipy.misc import imresize
+from PIL import Image
 
 
 class SceneChangeDetector:
@@ -123,7 +123,7 @@ def process_video(video_path, scd, writer, args):
                 lr_h = frames[k].shape[0] // args.scale_factor
                 lr_w = frames[k].shape[1] // args.scale_factor
                 frames[k] = frames[k][:lr_h * args.scale_factor, :lr_w * args.scale_factor]
-                frame_lr = imresize(frames[k], (lr_h, lr_w), interp='bicubic')
+                np.array(Image.fromarray(frames[k]).resize(size=(lr_h, lr_w), Image.Resampling.BICUBIC))
                 if args.blur:
                     frame_lr = cv2.GaussianBlur(frame_lr, (args.blur_size, args.blur_size), args.blur_sigma,
                                                 borderType=cv2.BORDER_REFLECT_101)
